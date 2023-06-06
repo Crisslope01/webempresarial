db3f690977ee48e64a46b651ebd7df900b4ba1f5
Crisslope01 <122842614+Crisslope01@users.noreply.github.com>
2023-06-06 11:43:10 -0400

    actualizacion escritorio 06/06

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,7 +1,8 @@
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 class PageListView(ListView):
@@ -13,4 +14,12 @@ class PageDetailView(DetailView):
 class PagesCreateView(CreateView):
     model = Page
     fields = ["title", "content", "order"]
- 
+    success_url = reverse_lazy('pages:pages') # reverse_lazy es para que no se ejecute hasta que se ejecute la vista
+    
+class PagesUpdateView(UpdateView):
+    model = Page
+    fields = ["title", "content", "order"]
+    template_name_suffix = '_update_form'
+    
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
