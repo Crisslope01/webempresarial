37d7d7718705e78f51ab88540bb38a1785769f4f
Crisslope01 <122842614+Crisslope01@users.noreply.github.com>
2023-06-17 15:23:00 -0400

    cambios note

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -138,10 +138,19 @@ LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
 
 #Emails servidor SMTP de pruebas
-if DEBUG:
+''' if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
     EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
     
 else:
     # Aqui hay que configurar un email real para produccion
-    pass
+    pass'''
+
+#Emails servidor SMTP de pruebas
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'emailpruebasdjango@gmail.com'
+EMAIL_HOST_PASSWORD = 'yyaiavgosmympflr'
+EMAIL_USE_TLS = True
